ab1f2602826c7c7bfd7681a8e158df8099b553d6
maki <128175965+makigero19@users.noreply.github.com>
2023-03-18 01:15:35 +0000

    product

---
+++ frontend/components/product.tsx
@@ -0,0 +1,11 @@
+
+import { useState } from "react"
+import styles from '../styles/Home.module.css'
+
+
+export default function () {
+   
+    return <div className={styles.content}>
+    
+        </div>
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <Layout title="home">
         <>       
         <Banner />
+        
         </>
 
       </Layout>
